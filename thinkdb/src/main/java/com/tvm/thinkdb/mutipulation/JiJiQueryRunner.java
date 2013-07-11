b1d17805275d3713795fa13e053348e980649f75
jiji879 <jiji_yu@163.com>
2013-07-11 16:35:34 +0800

    add batch insert method

--- thinkdb/src/main/java/com/tvm/thinkdb/mutipulation/JiJiQueryRunner.java
+++ thinkdb/src/main/java/com/tvm/thinkdb/mutipulation/JiJiQueryRunner.java
@@ -1,6 +1,5 @@
 package com.tvm.thinkdb.mutipulation;
 
-import com.tvm.thinkdb.util.ColumnPropertySupporter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.Connection;
@@ -10,10 +9,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
 import org.apache.commons.dbutils.BeanProcessor;
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
+
+import com.tvm.thinkdb.util.ColumnPropertySupporter;
 
 /**
  * 重写部分QueryRunner的方法,实现了orm的插入和查询
@@ -45,26 +48,89 @@ public class JiJiQueryRunner extends QueryRunner
 		this.processor = ResultSetProcessor.getProcessor(processorType);
 	}
 
-	/**
-	 * 向数据库中插入一条数据,参数通过sql指定,数据通过obj包含的对象指定
-	 * 
-	 * @param conn
-	 *            数据库连接
-	 * @param sql
-	 *            包含带设置参数的sql
-	 * @param obj
-	 *            包含了sql语句需要的数据对象
-	 * @return 返回插入sql执行的影响行数
-	 * @throws SQLException
-	 *             如果数据库操作发生异常或者插入的数据不合法则抛出
-	 * @author 余洪禹
-	 */
 	public <T> int insert(Connection conn, String sql, T obj) throws SQLException
 	{
-		Class<T> clazz = (Class<T>) obj.getClass();
 		PreparedStatement stat = conn.prepareStatement(sql);
-
 		List<String> fieldNames = parseUpdateSql(sql);
+		setStatParameter(stat, fieldNames, obj);
+		return stat.executeUpdate();
+	}
+
+	public <T> List<Integer> batchInsert(Connection conn, String sql, List<T> objList)
+			throws SQLException
+	{
+		List<Integer> results = new LinkedList<Integer>();
+		PreparedStatement stat = conn.prepareStatement(sql);
+		List<String> fieldNames = parseUpdateSql(sql);
+
+		if (objList != null && objList.size() != 0)
+		{
+			for (T obj : objList)
+			{
+				setStatParameter(stat, fieldNames, obj);
+				results.add(stat.executeUpdate());
+			}
+		}
+
+		return results;
+	}
+
+	public <T> T query(Connection conn, String sql, Class<T> clazz) throws SQLException
+	{
+		Statement stat = null;
+		ResultSet rs = null;
+		try
+		{
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+
+			return rs.next() ? this.processor.toBean(rs, clazz) : null;
+		}
+		finally
+		{
+			DbUtils.closeQuietly(null, stat, rs);
+		}
+	}
+
+	public <T> List<T> queryList(Connection conn, String sql, Class<T> clazz) throws SQLException
+	{
+		Statement stat = null;
+		ResultSet rs = null;
+		try
+		{
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+
+			return this.processor.toBeanList(rs, clazz);
+		}
+		finally
+		{
+			DbUtils.closeQuietly(null, stat, rs);
+		}
+	}
+
+	private List<String> parseUpdateSql(String sql)
+	{
+		try
+		{
+			int leftParenthese = sql.indexOf('(');
+			int rightParenthese = sql.substring(leftParenthese + 1).indexOf(')');
+
+			String[] fieldStr = sql.substring(leftParenthese + 1,
+					leftParenthese + rightParenthese + 1).split(",");
+			return Arrays.asList(fieldStr);
+		}
+		catch (Exception ex)
+		{
+		}
+		throw new IllegalArgumentException("请确保sql的语法正确性:" + sql);
+	}
+
+	private <T> void setStatParameter(PreparedStatement stat, List<String> fieldNames, T obj)
+			throws SQLException
+	{
+		Class<T> clazz = (Class<T>) obj.getClass();
+
 		ParameterMetaData paramMeta = stat.getParameterMetaData();
 		int paramCount = paramMeta.getParameterCount();
 		for (int i = 1; i <= paramCount; i++)
@@ -100,86 +166,5 @@ public class JiJiQueryRunner extends QueryRunner
 						columnName, getterName));
 			}
 		}
-
-		return stat.executeUpdate();
-	}
-
-	/**
-	 * 查询指定sql语句的实体对象
-	 * 
-	 * @param conn
-	 *            数据库连接
-	 * @param sql
-	 *            待执行sql语句
-	 * @param clazz
-	 *            需要转换的实体类对象
-	 * @return 数据库中指定记录的实体对象
-	 * @throws SQLException
-	 *             如果数据库操作发生异常或者进行实体关系转换时发生异常则抛出
-	 * @author 余洪禹
-	 */
-	public <T> T query(Connection conn, String sql, Class<T> clazz) throws SQLException
-	{
-		Statement stat = null;
-		ResultSet rs = null;
-		try
-		{
-			stat = conn.createStatement();
-			rs = stat.executeQuery(sql);
-
-			return rs.next() ? this.processor.toBean(rs, clazz) : null;
-		}
-		finally
-		{
-			DbUtils.closeQuietly(null, stat, rs);
-		}
-	}
-
-	/**
-	 * 查询指定sql语句的实体对象列表
-	 * 
-	 * @param conn
-	 *            数据库连接
-	 * @param sql
-	 *            待执行sql语句
-	 * @param clazz
-	 *            需要转换的实体类对象
-	 * @return 数据库返回记录的实体对象列表
-	 * @throws SQLException
-	 *             如果数据库操作发生异常或者进行实体关系转换时发生异常则抛出
-	 * @author 余洪禹
-	 */
-	public <T> List<T> queryList(Connection conn, String sql, Class<T> clazz) throws SQLException
-	{
-		Statement stat = null;
-		ResultSet rs = null;
-		try
-		{
-			stat = conn.createStatement();
-			rs = stat.executeQuery(sql);
-
-			return this.processor.toBeanList(rs, clazz);
-		}
-		finally
-		{
-			DbUtils.closeQuietly(null, stat, rs);
-		}
-	}
-
-	private List<String> parseUpdateSql(String sql)
-	{
-		try
-		{
-			int leftParenthese = sql.indexOf('(');
-			int rightParenthese = sql.substring(leftParenthese + 1).indexOf(')');
-
-			String[] fieldStr = sql.substring(leftParenthese + 1,
-					leftParenthese + rightParenthese + 1).split(",");
-			return Arrays.asList(fieldStr);
-		}
-		catch (Exception ex)
-		{
-		}
-		throw new IllegalArgumentException("请确保sql的语法正确性:" + sql);
 	}
 }
